3218cfbbc2879b51a082b3ef1539486f32627e3c
Anastasar <ereminaar83@mail.ru>
2022-12-22 20:52:31 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 100; 
+        int mile = x / 20;
+        System.out.println("Количество начисленных миль за билет:   " + mile);
+    }
+}
